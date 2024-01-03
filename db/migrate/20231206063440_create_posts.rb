5da01dded284ba4e436672cc2b2b224c89bdbbc3
ahmed025566 <ahmedsalah025566#gmail.com>
2024-01-03 18:11:09 +0200

    Fix migration error

--- db/migrate/20231206063440_create_posts.rb
+++ db/migrate/20231206063440_create_posts.rb
@@ -1,6 +1,6 @@
 class CreatePosts < ActiveRecord::Migration[7.1]
   def change
-    create_table :posts do |t|
+    create_table :articles do |t|
       t.text :text
       t.references :category, null: false, foreign_key: true
       t.text :hashes
